f4a655e04c53ac31fcc54d07ec78b97b322eab59
Christopher Ralph <chris.734c@gmail.com>
2024-09-08 22:07:03 -0400

    📦 NEW: Finished the restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,7 +1,19 @@
+const { JWT_SECRET } = require('../secrets');
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: 'token required'});
+  }
+  jwt.verify(token, JWT_SECRET, (error, decodedToken) => {
+    if (error) {
+      next({ status: 401, message: 'token invalid' });
+    } else {
+      req.decodedToken = decodedToken;
+      next();
+    }
+  })
   /*
     IMPLEMENT
 
