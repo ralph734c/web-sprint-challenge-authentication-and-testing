7186f4d48ccacce8370749563a29fd15fc64a8fd
Christopher Ralph <chris.734c@gmail.com>
2024-09-08 21:14:15 -0400

    📦 NEW: Fixed some dependencies, created a users model, set up some of auth router, added secrets.

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const { JWT_SECRET } = require('../secrets');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../users/users-model');
 
 router.post('/register', (req, res) => {
   res.end('implement register, please!');
---
+++ api/users/users-model.js
@@ -0,0 +1,20 @@
+const db = require('../../data/dbConfig');
+
+function find() {
+  return db('users');
+}
+
+function findById(id) {
+  return db('users').where('id', id).first();
+}
+
+async function add({ username, password }) {
+  const [id] = await db('users').insert({ username, password });
+  return findById(id);
+}
+
+module.exports = {
+  find,
+  findById,
+  add,
+}
