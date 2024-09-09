f3e151a2511da70442283a084fddc09ac7003b3e
Christopher Ralph <chris.734c@gmail.com>
2024-09-08 23:00:42 -0400

    📦 NEW: Created 4 passing tests for login and register

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,47 @@
+const request = require('supertest');
+const server = require('./server');
+const db = require('../data/dbConfig');
+const knex = require('knex');
+const knexConfig = require('../knexfile');
+
+beforeAll(async () => {
+  await db.migrate.latest();
+});
+
+afterAll(async () => {
+  await db.migrate.rollback();
+  await db.destroy();
+});
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
-})
+  expect(true).not.toBe(false);
+});
+
+describe('server.js', () => {
+  describe('[POST] to /api/auth/register', () => {
+    it('[1] the API responds with the correct status on successful registration', async () => {
+      const user = { username: 'Max', password: 'ChonkyBoy1$'}
+      const response = await request(server).post('/api/auth/register').send(user)
+      expect(response.status).toBe(201)
+    })
+    it('[2] the API responds with the correct username after successful registration', async () => {
+      const user = { username: 'Jynx', password: 'ImAdogMaybe3'}
+      const response = await request(server).post('/api/auth/register').send(user)
+      expect(response.body).toEqual(expect.objectContaining({ username: 'Jynx' }))
+    })
+  })
+  describe('[POST] to /api/auth/login', () => {
+    it('[3] the API responds with the correct message when provided with valid credentials', async () => {
+      const user = { username: 'Max', password: 'ChonkyBoy1$'}
+      const response = await request(server).post('/api/auth/login').send(user)
+      expect(response.body.message).toMatch(/Welcome, Max/i)
+    });
+    it('[4] the API responds with the correct status when invalid credentials are sent', async () => {
+      let response = await request(server).post('/api/auth/login').send({ username: 'Jynx', password: 'isThisThePass?'})
+      expect(response.status).toBe(401)
+      response = await request(server).post('/api/auth/login').send({ username: 'Jynxy-boy', password: 'ImAdogMaybe3'})
+      expect(response.status).toBe(401)
+    })
+  });
+});
